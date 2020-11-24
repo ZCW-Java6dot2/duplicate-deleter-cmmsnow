2324f1afdbdfeec0d48abf0dfa20c8605aa2c375
christine <christine@zipcodes-MBP-10.lan>
2020-11-24 15:55:02 -0500

    some tests passing

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -73,4 +75,17 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
         }
         return answer;
     }
+
+//    @Override
+//    public String[] removeDuplicates(int maxNumberOfDuplications) {
+//        return Arrays.stream(array).filter(y -> getOccurrence(y) < maxNumberOfDuplications).toArray(String[]::new);
+//    }
+//    public Long getOccurrence(String x){
+//        return Arrays.stream(array).filter(z -> z.equals(x)).count();
+//    }
+//
+//    @Override
+//    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+//        return Arrays.stream(array).filter(y -> getOccurrence(y) != exactNumberOfDuplications).toArray(String[]::new);
+//    }
 }
